54f9af82b2c614a5024d71bf0131c62ff87b644d
brian <brianmurigi19@gmail.com>
2024-04-15 06:48:12 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from sqlalchemy import asc
 
 from models import db, Message
 
@@ -14,13 +15,68 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        all_messages = []
 
-@app.route('/messages/<int:id>')
+        for message in Message.query.order_by(asc(Message.created_at)):
+            message_dict = message.to_dict()
+            all_messages.append(message_dict)
+
+        response = make_response(all_messages, 200)
+        return response
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(message_dict, 201)
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+
+    # if request.method == 'GET':
+    #     specific_message_dict = specific_message.to_dict()
+    #     response = make_response(specific_message_dict, 200)
+
+    #     return response
+    specific_message = Message.query.filter(Message.id == id).first()
+    
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in request.form:
+            setattr(specific_message, attr, data[attr])
+
+        db.session.add(specific_message)
+        db.session.commit()
+
+        specific_message_dict = specific_message.to_dict()
+
+        response = make_response(specific_message_dict, 200)
+        
+        return response
+        
+    elif request.method == 'DELETE':
+        db.session.delete(specific_message)
+        db.session.commit()
+
+        response_body = {
+            "deleted": True
+        }
+
+        response = make_response(response_body, 200)
+        return response
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
